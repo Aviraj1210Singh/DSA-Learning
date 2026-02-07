643fd4e5840d30a69021e14c9f87abd083b407fd
Aviraj Singh <aviraj1210singh@gmail.com>
2026-02-07 16:23:00 +0530

    Version2 of HelloWorld program

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld{
     public static void main(String[] arg){
-        System.out.print("Hello World");
+        System.out.println("Hello World");
+        System.out.print("I am learning DSA");
 
     }
     
