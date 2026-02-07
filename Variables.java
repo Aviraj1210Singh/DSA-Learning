b3878a46344594f01d46082538e1f5eb2926cc14
Aviraj Singh <aviraj1210singh@gmail.com>
2026-02-07 17:00:24 +0530

    I don't know what version this is going to be but it is created for learnin few Variables

---
+++ Variables.java
@@ -0,0 +1,19 @@
+public class Variables {
+
+
+    public static void main(String[] args) {
+        
+    String bestgame = "Elden Ring";
+    double priceofgame = 5999.99;
+    int age = 4;
+    char grade = 'A';
+    boolean peopleliked = true;
+
+    if(peopleliked){
+         System.out.println("The game is " + bestgame + ". It is " + age + " years old game." + " It is rated " + grade + ". And it costs " + priceofgame + ".");
+    }
+    else{ 
+        System.out.println("People didn't liked it very much.");
+    }
+    }
+}
