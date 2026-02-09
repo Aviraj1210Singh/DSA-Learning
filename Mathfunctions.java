b5d6e63951bb008e05636d66c9c239eb385f1202
Aviraj Singh <aviraj1210singh@gmail.com>
2026-02-09 19:14:58 +0530

    V1 Math functions

---
+++ Mathfunctions.java
@@ -0,0 +1,27 @@
+public class Mathfunctions {
+    public static void main(String[] var0){
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+
+        double a,b,c,d,e,f,g,h;
+
+        a = Math.pow(2, 5);// this will be 2^5
+        System.out.println(a);
+        b = Math.abs(-5);
+        System.out.println(b);// gives the moduls or scalar or simply positive value only
+        c = Math.sqrt(9);
+        System.out.println(c);
+        d = Math.round(4.678);
+        System.out.println(d);
+        e = Math.ceil(3.1);
+        System.out.println(e);
+        f = Math.floor(3.789);
+        System.out.println(f);
+        g = Math.max(10, 20);
+        System.out.println(g);
+        h = Math.min(10, 20);
+        System.out.println(h);
+    }
+
+    
+}
