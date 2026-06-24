44613a58471e462007605820cfcf4cdaec9bb6e4
Aviraj Singh <aviraj1210singh@gmail.com>
2026-06-24 20:22:24 +0530

    V2 2D Array: added two ways of writing a same 2D array

--- twoDArrays.java
+++ twoDArrays.java
@@ -4,11 +4,15 @@ public class twoDArrays {
         // 2D array : An array where each element is an array
         //            Usful for storing the matrix of data 
 
+        /*
         String[] fruits = {"apple", "orange", "banana"};
         String[] vegetable = {"potato", "onion", "carrot"};
         String[] meats = {"chicken", "pork", "egg", "fish"};
 
         String[][] groceries = {fruits, vegetable, meats};
+        */
+
+        String[][] groceries = {{"apple", "orange", "banana"}, {"potato", "onion", "carrot"}, {"chicken", "pork", "egg", "fish"}};
 
         groceries[0][2] = "watermelon";
 
