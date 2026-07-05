fb5c46e1c43b4be9828758f7c1f34af8ebf79e01
Aviraj Singh <aviraj1210singh@gmail.com>
2026-07-06 00:10:44 +0530

    Add Car and ClassesInJava class example

---
+++ 08 - Classes & Objects/ClassesInJava.java
@@ -0,0 +1,34 @@
+public class ClassesInJava {
+    public static void main(String[] args) {
+        
+        // Object = An entity that holds data (attributes)
+        //          and can perform actions (methods)
+        //          It is a reference data type
+
+        Car car = new Car();
+
+        System.out.println(car.make);
+        System.out.println(car.model);
+        System.out.println(car.year);
+        System.out.println(car.price);
+        System.out.println(car.isRunning);
+
+        car.isRunning = true;
+        System.out.println(car.isRunning); 
+
+        car.start();
+        System.out.println(car.isRunning);
+        car.stop();
+        System.out.println(car.isRunning);
+        car.drive();
+        car.brake();
+
+
+        // more than one variables
+        Car car1 = new Car();
+        Car car2 = new Car();
+
+        System.out.println(car1.make + " " + car1.model);
+        System.out.println(car2.make + " " + car2.model);
+    }
+}
