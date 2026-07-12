db6bf1170d3d4976428af0943f5162403ece2242
Aviraj Singh <aviraj1210singh@gmail.com>
2026-07-12 12:42:24 +0530

    Method Overloading in Java

---
+++ 11 - Inheritance/11.3 - Method Overriding/Fish.java
@@ -0,0 +1,10 @@
+public class Fish extends Animal{
+
+    void move(){
+        // This method overrides the parent method
+        System.out.println("This animal is swimming");
+    }
+        
+    
+    
+}
---
+++ 11 - Inheritance/11.3 - Method Overriding/MethodOverridingInJava.java
@@ -0,0 +1,22 @@
+public class MethodOverridingInJava {
+
+    public static void main(String[] args) {
+        
+        /*
+        Method overriding = When a subclass(child class) provides its own
+                            implementation of a method that is already defined in superclass(parent class).
+                            Allows for reusability and give specific implementations.        
+        */ 
+
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Fish fish = new Fish();
+
+        dog.move();
+        cat.move();
+        fish.move(); // Here we are using the overriden method
+
+
+    }
+    
+}
