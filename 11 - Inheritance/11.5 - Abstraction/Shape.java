21cc3e020bd7f1ad9cbe2cccd13372b5407a4eb9
Aviraj Singh <aviraj1210singh@gmail.com>
2026-07-14 13:49:07 +0530

    Abstract Class and abstract method in Java

--- 11 - Inheritance/11.5 - Abstraction/Shape.java
+++ 11 - Inheritance/11.5 - Abstraction/Shape.java
@@ -3,6 +3,10 @@ public abstract class Shape {
     // this class is abstract that means we can not create it's objects
 
     abstract double area(); // this is an ABSTRACT method, that means it must be defined/ overriden in the child classes
+    /* 
+    Abstract method: This method has no implementation in the parent class, 
+                     and every class must provide an implementation.
+    */
 
     void display(){ // This is a CONCRETE method
         System.out.println("THis ia a shape");
